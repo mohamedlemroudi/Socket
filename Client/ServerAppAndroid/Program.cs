75ccb42b29b54f367a973f6f5125ac6976b4a311
Mohamed <Lema>
2023-11-08 22:56:57 +0100

    Implementar el client1.

---
+++ Client/ServerAppAndroid/Program.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class CardGameServer
+{
+    static Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
+
+    static async Task Main(string[] args)
+    {
+        TcpListener server = null;
+
+        try
+        {
+            int port = 12345; // Puerto para la comunicación
+
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            Console.WriteLine($"Servidor iniciado en el puerto {port}");
+
+            while (true)
+            {
+                Console.WriteLine("Esperando un cliente...");
+                TcpClient client = await server.AcceptTcpClientAsync();
+                Console.WriteLine("Cliente conectado!");
+
+                // Recibir el nombre del cliente
+                NetworkStream nameStream = client.GetStream();
+                byte[] nameBuffer = new byte[1024];
+                int nameBytesRead = await nameStream.ReadAsync(nameBuffer, 0, nameBuffer.Length);
+                string playerName = Encoding.ASCII.GetString(nameBuffer, 0, nameBytesRead).Trim();
+
+                // Agregar al jugador a la lista
+                clients.Add(playerName, client);
+
+                // Iniciar un nuevo hilo para manejar la lógica del juego para este cliente
+                Task.Run(() => HandleClientGame(client, playerName));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error: {e}");
+        }
+        finally
+        {
+            server?.Stop();
+        }
+    }
+
+    static async void HandleClientGame(TcpClient client, string playerName)
+    {
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+        {
+            string lletra = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Jugador {playerName} te la lletra: {lletra}");
+
+            // Enviar la respuesta a otro jugador específico (reemplaza "recipientPlayerName" con el nombre del jugador destinatario)
+            if (clients.ContainsKey("mohamed"))
+            {
+                TcpClient recipientClient = clients["mohamed"];
+                NetworkStream recipientStream = recipientClient.GetStream();
+                byte[] responseBuffer = Encoding.ASCII.GetBytes($"El jugador {playerName} dijo: {lletra}");
+                await recipientStream.WriteAsync(responseBuffer, 0, responseBuffer.Length);
+            }
+        }
+
+        // Eliminar al jugador de la lista cuando se desconecta
+        clients.Remove(playerName);
+        client.Close();
+    }
+}
---
+++ Client/app/src/main/java/cat/boscdelacoma/appandroid/CardGameClient.kt
@@ -0,0 +1,43 @@
+package cat.boscdelacoma.appandroid
+
+import android.os.AsyncTask
+import android.util.Log
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.Socket
+
+class CardGameClient : AsyncTask<String, Void, Void>() {
+    override fun doInBackground(vararg params: String?): Void? {
+        try {
+            val serverIP = "172.23.3.161" // Reemplaza con la dirección IP del servidor
+            val serverPort = 12345
+
+            val socket = Socket(serverIP, serverPort)
+
+            val playerName = "mohamed" // Recibe el nombre del jugador como parámetro
+            val cardToGuess = "A" // Recibe la carta a adivinar como parámetro
+
+            // Enviar el nombre del jugador al servidor
+            val out = PrintWriter(socket.getOutputStream(), true)
+            out.println(playerName)
+
+            // Enviar la carta a adivinar al servidor
+            out.println(cardToGuess)
+
+            // Comienza a escuchar al servidor
+            val `in` = BufferedReader(InputStreamReader(socket.getInputStream()))
+            while (true) {
+                val respuesta = `in`.readLine()
+                if (respuesta != null) {
+                    // Procesa la respuesta del servidor, como mostrarla en la interfaz de usuario
+                    println(respuesta)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return null
+    }
+}
